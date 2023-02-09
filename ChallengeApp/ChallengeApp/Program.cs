c3f1e86f08e930978096e6065cec89119befa986
Piotr Piekarz <p.piekarz@outlook.com>
2023-02-09 10:35:19 +0100

    day 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var name = "Ewa";
+var sex = "kobieta";
+var age = 33;
+
+if (sex == "kobieta" && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (name == "Ewa" && sex == "kobieta" && age == 33)
+{
+    Console.WriteLine("Perfekcyjna kandydatka: Ewa, lat 33.");
+}
+else if (sex != "kobieta" && age < 18)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
