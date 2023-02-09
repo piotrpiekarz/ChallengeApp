e263f2169122d24bbd860cc08a947259d3019160
Piotr Piekarz <p.piekarz@outlook.com>
2023-02-09 13:41:47 +0100

    day 5

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,16 +1,58 @@
-﻿var name = "Ewa";
-var sex = "kobieta";
-var age = 33;
+﻿int number = 1004566;
+string numberAsString = number.ToString();
+char[] digits = numberAsString.ToArray();
 
-if (sex == "kobieta" && age < 30)
+int count0 = 0;
+int count1 = 0;
+int count2 = 0;
+int count3 = 0;
+int count4 = 0;
+int count5 = 0;
+int count6 = 0;
+int count7 = 0;
+int count8 = 0;
+int count9 = 0;
+
+foreach (char digit in digits)
 {
-    Console.WriteLine("Kobieta poniżej 30 lat");
+    if (digit == '0')
+    { count0++; }
+
+    else if (digit == '1')
+    { count1++; }
+
+    else if (digit == '2')
+    { count2++; }
+
+    else if (digit == '3')
+    { count3++; }
+
+    else if (digit == '4')
+    { count4++; }
+
+    else if (digit == '5')
+    { count5++; }
+
+    else if (digit == '6')
+    { count6++; }
+
+    else if (digit == '7')
+    { count7++; }
+
+    else if (digit == '8')
+    { count8++; }
+
+    else if (digit == '9')
+    { count9++; }
 }
-else if (name == "Ewa" && sex == "kobieta" && age == 33)
-{
-    Console.WriteLine("Perfekcyjna kandydatka: Ewa, lat 33.");
-}
-else if (sex != "kobieta" && age < 18)
-{
-    Console.WriteLine("Niepełnoletni mężczyzna");
-}
+Console.WriteLine("Ilość danych cyfr w liczbie " + number);
+Console.WriteLine("0 => " + count0);
+Console.WriteLine("1 => " + count1);
+Console.WriteLine("2 => " + count2);
+Console.WriteLine("3 => " + count3);
+Console.WriteLine("4 => " + count4);
+Console.WriteLine("5 => " + count5);
+Console.WriteLine("6 => " + count6);
+Console.WriteLine("7 => " + count7);
+Console.WriteLine("8 => " + count8);
+Console.WriteLine("9 => " + count9);
